29c897d4a539568221f62e60fa22fb20d44ff201
Sergei Kocharian <36566227+kachanix@users.noreply.github.com>
2019-06-26 13:44:33 +0300

    26.04 last try

--- src/lab4/core.clj
+++ src/lab4/core.clj
@@ -2,111 +2,120 @@
   (:import (clojure.lang Symbol)))
 (require '[riddley.walk :as rw])
 
-
 (defn recur-statement? [expr]
   (and (seq? expr)
        (= (first expr) 'my-recur)))
 
 (defn recur-expression [loop-finished-sym loop-bindings-sym node]
-
   `(do
      (reset! ~loop-finished-sym false)
      (reset! ~loop-bindings-sym
-             (vec (interleave (take-nth 2 @~loop-bindings-sym) (list ~@(rest node)))))
+             (vec (interleave (take-nth 2 @~loop-bindings-sym)
+                              (list ~@(rest node)))))
      nil))
 
 (defmacro my-loop
   [user-bindings & body]
-  (println (count user-bindings))
-  (println (nth user-bindings 3))
-  ;(println (nth user-bindings 3))
   (let [finished (gensym "finished")
         bindings (gensym "binding")
-        return-value (gensym "return-value")
-        post-exp (gensym "postexp")
-        all-counters (gensym "all-counters")
-        ]
-    `(let [~finished (atom false)
-           ~bindings (atom '~user-bindings)
+        return-value (gensym "return-value")                ;Создать уникальные
+        post-exp (gensym "postexp")                         ;названия для
+        all-iterators (gensym "all-iterators")              ;востребованных
+        arg1 (gensym "arg1")                                ;значений
+        arg2 (gensym "arg2")]
+    `(let [~finished (atom false)                           ;Указатель на завершение работы
+           ~bindings (atom '~user-bindings)                 ;Сочетание символа и его значения
            ~return-value (atom nil)
-           ~'break (delay (throw (ex-info nil {:type :break})))
-           ~'continue (delay (throw (ex-info nil {:type :continue})))
-           ~post-exp (atom nil)
-           ~all-counters (atom (conj [] (second @~bindings)))]
-       (println "eto counter" @~all-counters)
+           ~'break (delay (throw                            ; "Команда" завершения
+                            (ex-info nil {:type :break})))  ; работы цикла
+           ~'continue (delay
+                        (throw                              ; "Команда" перехода на
+                          (ex-info nil {:type :continue}))) ; следующую итерацию
+
+           ~post-exp (atom nil)                             ; Выражение, преобразующее итератор
+
+           ~all-iterators (atom                             ; Все прошедшие
+                            (conj [] (second @~bindings)))  ; итераторы
+
+           ~arg1 '~(nth user-bindings 2)                    ;см. for-loop
+           ~arg2 '~(nth user-bindings 3)]                   ;см. for-loop
+
        (loop [reducing-body# '~body]
-         (if (= (str (first reducing-body#)) "my-recur")
+         (if (= (str (first reducing-body#)) "my-recur")    ;получаем post-expression
            (reset! ~post-exp (last reducing-body#))
            (recur (last reducing-body#))))
+
        (try
          (while (not @~finished)
            (reset! ~finished true)
-           ;(println '~(conj '() (first(take-nth 2 user-bindings))))
            (try
              (reset! ~return-value
                      (apply
                        (fn ~(vec (conj '() (first user-bindings)))
-                         ~@(rw/walk-exprs recur-statement?
-                                          (partial recur-expression finished bindings) body))
-                       ;(take-nth 2 (rest @~bindings))
-                       (conj '() (second @~bindings))
-                       ))
+                         ~@(rw/walk-exprs                   ; выполнение body,
+                             recur-statement?               ; преобразование итератора
+                             (partial recur-expression finished bindings)
+                             body))
+                       (conj '() (second @~bindings))))
 
-             @~return-value
-
-             (reset! ~all-counters (conj @~all-counters (second @~bindings)))
-             (println "eto counter" @~all-counters)
-
-             (catch Exception e#
-               (reset! ~bindings [(first @~bindings) (@(resolve (first @~post-exp)) (second @~bindings))])
-               (when-not (= :continue (:type (ex-data e#)))
+             (reset! ~all-iterators                         ; добавление нынешнего
+                     (conj @~all-iterators                  ; итератора
+                           (second @~bindings)))
+             (catch Exception e#                            ; здесь ловится exception
+               (reset! ~bindings [(first @~bindings)        ; и проверяется его тип
+                                  (@(resolve                ; если это break - то кидаем
+                                      (first @~post-exp))   ; еще один exception в "верхний"
+                                    (second @~bindings))])  ; блок try/catch, иначе -
+               (when-not (= :continue (:type (ex-data e#))) ; переходим к следующей итерации
                  (throw e#))
                (reset! ~finished false)
-               (reset! ~all-counters
-                       (assoc @~all-counters
-                         (dec (count @~all-counters))
-                         (@(resolve (first @~post-exp)) (last @~all-counters))))
-               )))
-         (catch Exception e#
-           (when-not (= :break (:type (ex-data e#)))
-             (throw e#))
-           @~all-counters))
+               (reset! ~all-iterators
+                       (assoc @~all-iterators
+                         (dec (count @~all-iterators))
+                         (@(resolve (first @~post-exp))
+                           (last @~all-iterators)))))))
+         (catch Exception e#                                ; "верхний" блок try/catch,
+           (when-not (= :break (:type (ex-data e#)))        ; предназначенный для того,
+             (reset! ~all-iterators                         ; чтобы "ловить" команду "break"
+                     (conj @~all-iterators 0)))))
 
+       (if (= (last @~all-iterators) (last (drop-last @~all-iterators)))
+         (reset! ~all-iterators (vec (drop-last (drop-last @~all-iterators))))
+         (reset! ~all-iterators (vec (drop-last @~all-iterators))))
 
-       (if (= nil (nth ~user-bindings 2))
-         (if (= (last @~all-counters) (last (drop-last @~all-counters)))
-           (vec (drop-last @~all-counters))
-           @~all-counters)
-         (if (= (nth ~user-bindings 2) reduce)
-           ((nth ~user-bindings 2) (nth ~user-bindings 3) @~all-counters)
-           (if (= (nth ~user-bindings 3) nil)
-             (transduce (nth ~user-bindings 2) conj  @~all-counters)
-             (transduce (nth ~user-bindings 2) (nth ~user-bindings 3)  @~all-counters))))
-       ;(transduce (nth user-bindings 2) (nth user-bindings 3) all-counters)
-       )))
-
-;(def a (map inc))
-;
-;(println (transduce a conj (range 5)))
-
+       ;(println "all iteration elements" @~all-iterators)
+       (if (= nil ~arg1)                                    ; применение нужной функции
+         @~all-iterators                                    ; к all-iterators в зависимости
+         (if (= (str ~arg1) "reduce")                       ; от переданных аргументов
+           (reduce (resolve ~arg2) @~all-iterators)         ; arg1 и arg2
+           (if (= ~arg2 nil)
+             (transduce ~(nth user-bindings 2) conj @~all-iterators)
+             (transduce ~(nth user-bindings 2) (resolve ~arg2)
+                        @~all-iterators)))))))
 
 (defmacro for-loop
+  ;;;;; arg1 и arg2 передаются в функцию, чтобы понять, какое действие совершить
+  ;;;;; над итоговым значением. Изначально возвращается список пройденных
+  ;;;;; значений итератора(для получения требуется отправить nil nil). Чтобы применить
+  ;;;;; map отправляется (map *action*) + nil или */-+. Фильтрация - идентично
+  ;;;;; (filter *predicate*) + nil или */-+. Для свертки - reduce + +/*-.
 
-  ;;;It's for a global one
-  ([symbol# check-exp post-exp body]
+  ;;;Реализация  цикла с глобальной константой
+  ([symbol# check-exp post-exp
+    arg1 arg2
+    body]
    (let [body-tmp (concat body (list (list 'my-recur post-exp)))
-         ;symbol# (second check-exp)
-         init-value @(resolve (symbol symbol#))]
+         init-value @(resolve symbol#)]
      `(my-loop [~symbol# ~init-value
-                ;(map inc)
-                (map inc)  nil
-                ]
+                ~arg1 ~arg2]
                (if ~check-exp
                  (do
                    ~@body-tmp)))))
 
-  ;;;It's for range
-  ([symbol range body]
+  ;;;Реализация с (range)
+  ([symbol range
+    arg1 arg2
+    body]
    (let [init-val (atom nil)
          post-exp (atom nil)
          check-exp (atom nil)]
@@ -127,92 +136,45 @@
      (let [body-tmp (concat body (list (list 'my-recur @post-exp)))
            check-expression @check-exp
            initial-value @init-val]
-       `(my-loop [~symbol ~initial-value]
+       `(my-loop [~symbol ~initial-value
+                  ~arg1 ~arg2]
                  (if ~check-expression
                    (do
                      ~@body-tmp))))))
 
-  ;;;This one is for default
-  ([symbol init-value check-exp post-exp body]
+  ;;;Обычный (for j := 7; j <= 9; j++)
+  ([symbol init-value check-exp post-exp
+    arg1 arg2
+    body]
    (let [body-tmp (concat body (list (list 'my-recur post-exp)))]
-     (println body)
-     `(my-loop [~symbol ~init-value]
+     `(my-loop [~symbol ~init-value
+                ~arg1 ~arg2]
                (if ~check-exp
                  (do
-                   ~@body-tmp)))))
-  )
+                   ~@body-tmp))))))
 
-;(println (macroexpand
-;           (for-loop i 10 (> i 2) (dec i)
-;                     (
-;                       ;(if (= i 3) @break)
-;                       (if (= i 5) @continue)
-;                       (println i)))))
-;
-;(println (macroexpand
-;  (for-loop i (range 2 10)
-;            ((if (= i 3) @continue)
-;              (if (= i 7) @break)
-;(println i)))))
-;;
-(def a 1)
-(println (macroexpand
-           (for-loop a (< a 10) (inc a)
-                     ((if (= a 3) @continue)
-                       (if (= a 8) @break)
-                       (println a)))))
-
-(defmacro for-loop-with-global-variable
-  [[symbol# check-exp post-exp] & body]
-  (let [body-tmp (concat body (list (list 'my-recur post-exp)))
-        ;symbol# (second check-exp)
-        init-value @(resolve (symbol symbol#))
-        ]
-    `(my-loop [~symbol# ~init-value]
-              (if ~check-exp
-                (do
-                  ~@body-tmp)))))
-
-(defmacro for-in-range
-  [[symbol range] & body]
-  (let [init-val (atom nil)
-        post-exp (atom nil)
-        check-exp (atom nil)]
-    (if (= (count range) 2)
-      (do
-        (reset! init-val 0)
-        (reset! post-exp `(inc ~symbol))
-        (reset! check-exp `(< ~symbol ~(nth range 1)))
-        )
-      (do
-        (reset! init-val (nth range 1))
-        (reset! post-exp (if (> (nth range 1) (nth range 2))
-                           `(dec ~symbol)
-                           `(inc ~symbol)))
-        (reset! check-exp (if (> (nth range 1) (nth range 2))
-                            `(> ~symbol ~(nth range 2))
-                            `(< ~symbol ~(nth range 2))))))
-    (let [body-tmp (concat body (list (list 'my-recur @post-exp)))
-          check-expression @check-exp
-          initial-value @init-val]
-      `(my-loop [~symbol ~initial-value]
-                (if ~check-expression
-                  (do
-                    ~@body-tmp))))))
+(def i 1)
+(println "macro output is : "
+         (macroexpand
+           (for-loop i (< i 20) (inc i)
+                     reduce +
+                     ((if (= i 3) @continue)
+                       (if (= i 8) @break)
+                       (println i)))))
 
 
+(println "macro output is : "
+         (macroexpand
+           (for-loop i 10 (> i 2) (dec i)
+                     (map inc) +
+                     ((if (= i 5)
+                        @continue)
+                       (println i)))))
 
-
-
-;(defmacro x
-;  ([a]
-;   `(println "huy"))
-;  ([b c]
-;   `(println "zalupa")))
-;(x 1)
-;(x 2 2)
-;
-
-;
-;(println (range '(1 2 34
-;                   )))
+(println "macro output is : "
+         (macroexpand
+           (for-loop i (range 2 10)
+                     (map inc) nil
+                     ((if (= i 3) @continue)
+                       (if (= i 7) @break)
+                       (println i)))))
--- src/playground/pg.clj
+++ src/playground/pg.clj
@@ -4,3 +4,124 @@
 (def a (map inc))
 
 (println (transduce a conj (range 5)))
+(let*
+  [finished399 (clojure.core/atom false)
+   binding400 (clojure.core/atom (quote [i 10 (map inc) +]))
+   return-value401 (clojure.core/atom nil)
+   break (clojure.core/delay
+           (throw (clojure.core/ex-info nil {:type :break})))
+   continue (clojure.core/delay
+              (throw (clojure.core/ex-info nil {:type :continue})))
+   postexp402 (clojure.core/atom nil)
+   all-counters403 (clojure.core/atom
+                     (clojure.core/conj
+                       []
+                       (clojure.core/second (clojure.core/deref binding400))))
+   arg1404 (quote (map inc))
+   arg2405 (quote +)]
+
+  (clojure.core/loop
+    [reducing-body__365__auto__ (quote ((if (> i 2)
+                                          (do (if (= i 5)
+                                                (clojure.core/deref continue))
+                                              (println i)
+                                              (my-recur (dec i))))))]
+    (if (clojure.core/=
+          (clojure.core/str (clojure.core/first reducing-body__365__auto__))
+          my-recur)
+      (clojure.core/reset! postexp402
+                           (clojure.core/last reducing-body__365__auto__))
+      (recur (clojure.core/last reducing-body__365__auto__))))
+  (try
+    (clojure.core/while
+      (clojure.core/not (clojure.core/deref finished399))
+      (clojure.core/reset! finished399 true)
+      (try
+        (clojure.core/reset! return-value401
+                             (clojure.core/apply
+                               (clojure.core/fn [i]
+                                 (if (. clojure.lang.Numbers
+                                        (clojure.core/gt i 2))
+                                   (do
+                                     (if
+                                       (. clojure.lang.Util
+                                          clojure.core/equiv i 5)
+                                       (clojure.core/deref continue))
+                                     (println i)
+                                     (do
+                                       (clojure.core/reset! finished399 false)
+                                       (clojure.core/reset!
+                                         binding400
+                                         (clojure.core/vec
+                                           (clojure.core/interleave
+                                             (clojure.core/take-nth
+                                               2
+                                               (clojure.core/deref binding400))
+                                             (clojure.core/list (dec i)))))
+                                       nil))))
+                               (clojure.core/conj
+                                 (quote ())
+                                 (clojure.core/second
+                                   (clojure.core/deref binding400)))))
+        (clojure.core/reset!
+          all-counters403
+          (clojure.core/conj
+            (clojure.core/deref all-counters403)
+            (clojure.core/second (clojure.core/deref binding400))))
+        (catch java.lang.Exception e__366__auto__
+          (clojure.core/reset!
+            binding400
+            [(clojure.core/first (clojure.core/deref binding400))
+             ((clojure.core/deref
+                (clojure.core/resolve
+                  (clojure.core/first (clojure.core/deref postexp402))))
+               (clojure.core/second (clojure.core/deref binding400)))])
+          (clojure.core/when-not
+            (clojure.core/= :continue
+                            (:type (clojure.core/ex-data e__366__auto__)))
+            (throw e__366__auto__))
+          (clojure.core/reset! finished399 false)
+          (clojure.core/reset! all-counters403
+                               (clojure.core/assoc
+                                 (clojure.core/deref all-counters403)
+                                 (clojure.core/dec
+                                   (clojure.core/count
+                                     (clojure.core/deref all-counters403)))
+                                 ((clojure.core/deref
+                                    (clojure.core/resolve
+                                      (clojure.core/first
+                                        (clojure.core/deref postexp402))))
+                                   (clojure.core/last
+                                     (clojure.core/deref all-counters403))))))))
+    (catch java.lang.Exception e__366__auto__
+      (clojure.core/when-not
+        (clojure.core/= :break (:type (clojure.core/ex-data e__366__auto__)))
+        (clojure.core/reset! all-counters403
+                             (clojure.core/conj
+                               (clojure.core/deref all-counters403)
+                               0)))))
+  (if (clojure.core/=
+        (clojure.core/last (clojure.core/deref all-counters403))
+        (clojure.core/last (clojure.core/drop-last
+                             (clojure.core/deref all-counters403))))
+    (clojure.core/reset! all-counters403
+                         (clojure.core/vec
+                           (clojure.core/drop-last
+                             (clojure.core/drop-last
+                               (clojure.core/deref all-counters403)))))
+    (clojure.core/reset!
+      all-counters403
+      (clojure.core/vec
+        (clojure.core/drop-last (clojure.core/deref all-counters403)))))
+  (clojure.core/println all iteration elements
+                        (clojure.core/deref all-counters403))
+  (if (clojure.core/= nil arg1404)
+    (clojure.core/deref all-counters403)
+    (if (clojure.core/= (clojure.core/str arg1404) reduce)
+      (clojure.core/reduce (clojure.core/resolve arg2405)
+                           (clojure.core/deref all-counters403))
+      (if (clojure.core/= arg2405 nil)
+        (clojure.core/transduce (map inc) clojure.core/conj
+                                (clojure.core/deref all-counters403))
+        (clojure.core/transduce (map inc) (clojure.core/resolve arg2405)
+                                (clojure.core/deref all-counters403))))))
